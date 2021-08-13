1dbf28ce66a2fa77132dfe1be06a2e8882fc8146
Majklikikik <michaelstanek97@gmail.com>
2021-08-13 10:53:20 +0200

    Delete itemstacksizes.lua

--- computer/5/itemstacksizes.lua
+++
@@ -1,6 +0,0 @@
-function getStackSize(name)
-	if name=="minecraft:diamond" then return 64 end
-	if name=="minecraft:stick" then return 64 end
-	logger.log("Itemstacksizes: Unknown item: "..name)
-	return 64
-end
